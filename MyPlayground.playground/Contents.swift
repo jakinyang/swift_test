27ec87a67549a9438b4f681659ccdf4860349cb1
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-17 09:43:43 -0800

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Variables v. constants
+
+var highScore: Int = 0
+
+highScore = 55
+
+let myName: String = "John"
+
+var ages = [22, 33, 44, 1, 5, 81, 47, 1875]
+
+ages.count
+ages.last
+ages.append(99)
+print(ages)
---
+++ new_test_playground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let valueOne: Int = 22
+let valueTwo: Int = 44
+
+let sum = valueOne == valueTwo
