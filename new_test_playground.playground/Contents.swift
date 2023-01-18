986a8c8f4beb0dc531627296086fd8ac867c1dcf
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-18 08:46:44 -0800

    --add more test learning code

--- new_test_playground.playground/Contents.swift
+++ new_test_playground.playground/Contents.swift
@@ -1,6 +1,24 @@
 import UIKit
 
-let valueOne: Int = 22
-let valueTwo: Int = 44
+var ages: [Int] = []
+ages.sort()
 
-let sum = valueOne == valueTwo
+if let oldestAge = ages.last {
+    print("The oldest age is \(oldestAge)")
+} else {
+    print("the array is empty")
+}
+
+// Nil coalescing
+//let oldestAge = ages.last ?? 999
+
+// Guard Statement
+func getOldestAge() {
+    guard let oldestAge = ages.last else {
+        return
+    }
+    print("\(oldestAge) is oldest age.")
+}
+
+ 
+getOldestAge()
