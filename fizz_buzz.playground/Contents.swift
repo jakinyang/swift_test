7195011b5b0fe0deafabc01e8ffbc649e886c312
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-19 10:35:46 -0800

    --complete fizz buzz

---
+++ fizz_buzz.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func fizzBuzzer() {
+    for i in 1...100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print("FizzBuzz")
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else if i % 3 == 0 {
+            print("Fizz")
+        } else {
+            print(i)
+        }
+    }
+}
+
+fizzBuzzer()
