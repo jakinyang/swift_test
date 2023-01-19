0dc7da7484a7273f0bf67f0dad58a19d8f0007b8
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-19 08:09:41 -0800

    --add changes to swift

---
+++ swift_test_app/swift_test_app/swift_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_test_appApp.swift
+//  swift_test_app
+//
+//  Created by Jae Chun Yang on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct swift_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
