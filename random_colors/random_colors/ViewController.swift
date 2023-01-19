6a0d1bd0626bca344ae24fda04db27b984435660
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-19 10:37:37 -0800

    --init random colors app

---
+++ random_colors/random_colors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random_colors
+//
+//  Created by Jae Chun Yang on 2023-01-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
